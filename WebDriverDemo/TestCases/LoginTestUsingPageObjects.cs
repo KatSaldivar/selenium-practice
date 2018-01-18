02c3d34125f4669b67efc9f5c45d246374f4934d
KatSaldivar <katherinekaufmann1@gmail.com>
2018-01-17 18:34:17 -0800

    remove ignored files

--- WebDriverDemo/TestCases/LoginTestUsingPageObjects.cs
+++ WebDriverDemo/TestCases/LoginTestUsingPageObjects.cs
@@ -9,26 +9,28 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.PageObjects;
 using System.Configuration;
+using WebDriverDemo.WrapperFactory;
 
-namespace WebDriverDemo.PageObjects
+namespace WebDriverDemo.TestCases
 {
     class LoginTestUsingPageObjects
     {
-  
-            [Test]
+        //http://toolsqa.com/selenium-c-sharp/?lipi=urn%3Ali%3Apage%3Ad_flagship3_messaging%3BJxOADBYURbyJrpH5uuKTtQ%3D%3D
+
+        [Test]
             public void PageObjectLoginTest()
             {
+            
+                BrowserFactory.InitBrowser("Chrome");
+                BrowserFactory.LoadApplication(ConfigurationManager.AppSettings["URL"]);
 
-                IWebDriver driver = new ChromeDriver();
-                driver.Url = ConfigurationManager.AppSettings["URL"];
-
-                var homePage = new HomePage(driver);
+                var homePage = new HomePage(WrapperFactory.BrowserFactory.driver);
                 homePage.ClickOnMyAccount();
 
-                var loginPage = new LoginPage(driver);
+                var loginPage = new LoginPage(WrapperFactory.BrowserFactory.driver);
                 loginPage.LoginToApplication("LogInTest");
-
-                driver.Close();
+            
+                BrowserFactory.CloseAllDrivers();
         }
 
         
---
+++ WebDriverDemo/WrapperFactory/BrowserFactory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+
+
+namespace WebDriverDemo.WrapperFactory
+{
+    class BrowserFactory
+    {
+        private static readonly IDictionary<string, IWebDriver> Drivers = new Dictionary<string, IWebDriver>();
+        public static IWebDriver driver; //HAD TO MAKE THIS PUBLIC FOR TEST FILE. WHY???
+
+        public static IWebDriver Driver
+        {
+            get
+            { //THIS KEPT ERRORING OUT BEFORE RUNNING, IT WORKED AFTER COMMENTING OUT. WHY???
+                //if (driver == null)
+                    //throw new NullReferenceException("The WebDriver browser instance was not initialized. You should first call the method InitBrowser.");
+                return driver;
+            }
+            private set
+            {
+                driver = value;
+            }
+        }
+
+        public static void InitBrowser(string browserName)
+        {
+            switch (browserName)
+            {
+                case "Firefox": //DOESN'T WORK!!!
+                    if (Driver == null)
+                    {
+                        driver = new FirefoxDriver(@"C:\Users\Katie\Documents\selenium\geckodriver");
+                        Drivers.Add("Firefox", Driver);
+                    }
+                    break;
+
+                case "IE": //DOESN'T WORK!!!
+                    if (Driver == null)
+                    {
+                        driver = new InternetExplorerDriver(@"C:\Users\Katie\Documents\selenium\IEDriverServer");
+                        Drivers.Add("IE", Driver);
+                    }
+                    break;
+
+                case "Chrome":
+                    if (Driver == null)
+                    {
+                        driver = new ChromeDriver();
+                        Drivers.Add("Chrome", Driver);
+                    }
+                    break;
+            }
+        }
+
+        public static void LoadApplication(string url)
+        {
+            Driver.Url = url;
+        }
+
+        public static void CloseAllDrivers()
+        {
+            foreach (var key in Drivers.Keys)
+            {
+                Drivers[key].Close();
+                Drivers[key].Quit();
+            }
+        }
+    }
+}
